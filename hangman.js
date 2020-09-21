3c39a06a8ba043b06cf7b69e0cd35e9f08185750
Nickolaj <nickolaj99@outlook.com>
2020-09-21 12:22:13 +0200

    added keypress valid

--- hangman.js
+++ hangman.js
@@ -1,64 +1,70 @@
-var wordHintP = document.getElementById("wordHint")
+var options = ["word1", "word2", "word3"];
 
-var options = []
+var randomPick = options[Math.floor(Math.random()*options.length)];
 
+var word = randomPick.split([]);
 
+var userArray = [];
 
-function randomWord() {
-  options = ["Pik", "plankton", "sandyhook", "pleb", "columbine", "noggenfogger"];
-  var randomPick = options [Math.floor(Math.random()*options.length)];
+var length = word.length;
 
-  var word = randomPick.split([]);
-  console.log(word);
-  wordHint(word);
-}
+window.onload = function() {
+  for (var i = 0; i < length; i++) {
+    word[i] = "_"
+  };
+  document.getElementById("display").innerHTML = word;
+};
 
-
-function wordHint(word) {
-  for (var i = 0; i < options.length; i++) {
-    wordHint.innerHTML +=
-  }
-}
+var guesses = 15;
 
 
 
 
 
 
-randomWord();
+var allowedChar = ["a","b","c","d","e","f","g","h","i", "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","æ","ø","å"]
+var keypress;
 
-
-
-
-/*
-
-let text = document.getElementById("text");
-let btn = document.getElementById("btn");
-console
-let array = []
 
 const doSomething = function(){
-  btn.addEventListener("click", stringToArray);
+  document.addEventListener("keypress", charCheck);
 
+};
+
+function charCheck(e) {
+
+  keypress = e.textContent = e.key;
+
+  console.log(keypress)
+
+  if (allowedChar.includes(e.textContent = e.key) === true) {
+    wordHint(keypress)
+  };
 };
 
 
 
-function stringToArray() {
-  for (var i = 0; i < text.value.length; i++) {
-    let x = text.charAt(0)
-    array.push(x)
+function wordHint(e) {
+  var userInput = keypress;
+  console.log(userInput)
+  if (e.textContent += ` ${e.key}`) {
+    guesses--;
+
+    userArray.push(userInput);
+    console.log(userArray)
+
+    if (guesses === 0) {
+      alert("Game over")
+    }
+
+    if (userArray.length === word.length) {
+      alert("you win")
+    }
   }
-  console.log(array)
+
+  document.getElementById("userInput").innerHTML = userArray;
+  document.getElementById("guesses").innerHTML = guesses;
 }
-
-
-
-
-
-
-
-
 
 
 
@@ -67,5 +73,3 @@ function stringToArray() {
 
 
 window.addEventListener('load', doSomething);
-
-*/
