90780a4af2077d505fc75a90b083e90db383b5a1
Nickolaj <nickolaj99@outlook.com>
2020-09-22 11:16:33 +0200

    adds all same letters

--- hangman.js
+++ hangman.js
@@ -11,12 +11,13 @@ var userArray = [];
 
 var length = word.length;
 
-
-var guesses = 15;
+var guesses = 7;
 
 var keypress;
 
 var indexes = []
+
+var allowedChar = ["a","b","c","d","e","f","g","h","i", "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","æ","ø","å"];
 
 // Adder eventlistener til keyboard
 const doSomething = function(){
@@ -35,7 +36,7 @@ function populate() {
 
 
 function charCheck(e) {
-  var allowedChar = ["a","b","c","d","e","f","g","h","i", "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","æ","ø","å"];
+
   keypress = e.textContent = e.key;
 
   // Hvis keypressed er en af de tillate tegn
@@ -51,47 +52,31 @@ function charCheck(e) {
 
       userArray.push(userInput);
 
-
+      let removeKey = allowedChar.indexOf(userInput)
+      allowedChar.splice(removeKey, 1)
 
         if (word.includes(userInput)) {
 
 
-          indexes.push(word.indexOf(userInput))
+
+        //  var index = word.indexOf(userInput);
 
           for (var i = 0; i < word.length; i++) {
-
-            var index = word.indexOf(userInput);
-
-            wordU[index] = userInput
-
-
+            if(word[i] === userInput) {
+              wordU[i] = userInput
+            };
           };
 
-
-
-
           document.getElementById("display").innerHTML = wordU;
-
-          console.log(indexes)
-          console.log(index)
-
 
 
         } else {
             guesses--;
-        };
-
-
-
-
+    };
 
   document.getElementById("userInput").innerHTML = userArray;
   document.getElementById("guesses").innerHTML = guesses;
 };
-
-
-
-
 
 
 window.addEventListener('load', doSomething);
