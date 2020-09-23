f15148c5d87349577802f78eb71f2476d6071f24
jandersen747 <jonandersenofficial@hotmail.com>
2020-09-23 10:33:32 +0200

    guessnumberdone

--- execution.js
+++ execution.js
@@ -1,20 +1,21 @@
 'use strict';
 const isValid = function(e) {
     let name = document.formal.name;
+    let score = document.formal.score;
 
-    if (name.value === "") { // hvis born eller name ikke er udfyldt, kan det ikke submittes //
+    if (name.value === "" || score.value === "") { // hvis born eller name ikke er udfyldt, kan det ikke submittes //
         window.alert("Both fields must be filled in");
         name.focus();
         e.preventDefault(); // preventer submit //
         return false;
     } else {
-        createCookie(name.value, 42, 0.00094444); // name.value = navnet angivet, born = datoen, 42 = værdien, 0.00.. = hvor lang tid cookien er aktiveret //
+        createCookie(name.value, score.value, 0.00094444); // name.value = navnet angivet, born = datoen, 42 = værdien, 0.00.. = hvor lang tid cookien er aktiveret //
         return true;
     }
 }
 
 const getstarted = function() {
-    console.log(`Cookie niels1945-11-25? ${readCookie('niels1945-11-25')}`);
+    console.log(`Cookie name=score? ${readCookie('name=score')}`);
     console.log(`Cookies: ${document.cookie}`)
     document.formal.addEventListener('submit', isValid);
 }
--- guessnumber.js
+++ guessnumber.js
@@ -5,13 +5,26 @@ let showCount = document.getElementById("showcount");
 let timeNow;
 let timeThen;
 
+function startCount() {
+    timeNow = (new Date()).getTime();
+}
+
+/*
+function endCount() {
+    timeThen = (new Date()).getTime();
+    console.log("your time spend is " + (timeThen - timeNow) * 0.001 + " seconds");
+    console.log("Your score is " + ((timeThen - timeNow) * 0.01 + (i * 100)));
+}
+*/
 
 function inputFunc() {
     let b = document.getElementById("inputnumber").value;
+    timeThen = (new Date()).getTime();
 
     while (b !== a) {
         if (b == a) {
             document.getElementById("showresult").innerHTML = ("You win!");
+            document.getElementById("showScore").innerHTML = "Your score is: " + ((timeThen - timeNow) * 0.01 + (i * 1000));
           break;
 
         } else if (b < 1 || b > 1000) {
@@ -39,6 +52,8 @@ function inputFunc() {
 
     console.log("guess the number: " + a);
 
+
+
     function startCount() {
         timeNow = (new Date()).getTime();
     }
@@ -49,7 +64,17 @@ function inputFunc() {
         console.log("Your score is " + ((timeThen - timeNow) * 0.01 + (i * 100)));
     }
 
+    /*
+    function showScore() {
+        if (b == a)
+            document.getElementById("showScore").innerHTML = ((timeThen - timeNow) * 0.01 + (i * 100));
+        else (b !== a)
+            document.getElementById("showScore").innerHTML = "You need to win the game to display score";
+    }
+    */
+/*
 function btnSave() {
     let nameSave = document.getElementById("nameSave").value;
     console.log(nameSave);
 }
+*/
